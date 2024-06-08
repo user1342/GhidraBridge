3a3922bc6f3336d34ec364c6fc4ce9371a132828
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-06-08 15:08:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     author="James Stevenson",
     author_email="opensource@JamesStevenson.me",
     description="A Python interface for automating Ghidra tasks.",
-    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/user1342/GhidraBridge",
     packages=find_packages(),
